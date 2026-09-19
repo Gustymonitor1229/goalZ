6c0787a2da581480e2666cfc0090dd30e3acdd35
Bokang <molejaneonkarabile29@gmail.com>
2026-09-20 00:22:14 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='goalz-offline-v4';
+const CACHE_NAME='goalz-offline-v5';
 const APP_FILES=['./','./index.html','./manifest.webmanifest','./goalz-icon.svg','https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_FILES)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
